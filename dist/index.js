26a2d1c9125aef5f31feff926658e0ef2fb108d7
Grig Alex <alexengrigdev@gmail.com>
2020-08-21 18:23:46 +0300

    Update LICENSE

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 Endless-slice contributors
+ * Copyright 2020 Alexengrig Dev.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -28,7 +28,7 @@ function _interopRequireDefault(obj) {
 }
 
 /*
- * Copyright 2019 Endless-slice contributors
+ * Copyright 2020 Alexengrig Dev.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
